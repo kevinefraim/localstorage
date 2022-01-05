c2d15fba603fa4a5222251dcdd0fdc779bedbd2b
Kevin Efraim <kevinefraim02@gmail.com>
2022-01-05 18:38:51 -0300

    final

--- src/components/Productos/Productos.jsx
+++ src/components/Productos/Productos.jsx
@@ -10,6 +10,12 @@ const Agregar = ({ productos, setProductos }) => {
   const removeAll = () => {
     setProductos([]);
   };
+
+  const productsTotal = productos.reduce(
+    (price, product) => price + product.price * product.quantity,
+    0
+  );
+
   return (
     <div className="container mt-4">
       <h2>Productos</h2>
@@ -19,20 +25,27 @@ const Agregar = ({ productos, setProductos }) => {
       <button className="btn btn-danger" onClick={removeAll}>
         Eliminar todo
       </button>
+      {productos.length > 0 && (
+        <div className="mt-3">
+          <h3>Total: {productsTotal}</h3>
+        </div>
+      )}
 
       <div className="row border-top border-bottom mt-4 p-2">
         <div className="col-2 fw-bolder">#</div>
-        <div className="col-5 fw-bolder">Nombre del producto</div>
+        <div className="col-3 fw-bolder">Nombre del producto</div>
         <div className="col-2 fw-bolder">Cantidad</div>
         <div className="col-2 fw-bolder">Precio</div>
+        <div className="col-2 fw-bolder">Subtotal</div>
         <div className="col-1 fw-bolder">Acción</div>
       </div>
       {productos.map((item) => (
         <div className="row border-top border-bottom mt-4 p-2" key={item.id}>
           <div className="col-2 fw-bolder">{item.id}</div>
-          <div className="col-5 fw-bolder">{item.name}</div>
+          <div className="col-3 fw-bolder">{item.name}</div>
           <div className="col-2 fw-bolder">{item.quantity}</div>
           <div className="col-2 fw-bolder">{item.price}</div>
+          <div className="col-2 fw-bolder">{item.price * item.quantity}</div>
           <div className="col-1 fw-bolder">
             <button
               className="btn btn-danger"
